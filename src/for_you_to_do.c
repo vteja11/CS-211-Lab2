f3123032023e962c763b7edbb0918b2c5245b790
Teja <vemparalateja18@gmail.com>
2020-10-31 18:21:19 -0700

    updates for bug fixes

--- src/for_you_to_do.c
+++ src/for_you_to_do.c
@@ -1,9 +1,9 @@
 #include "../include/for_you_to_do.h"
 
-int get_block_size(i){
+int get_block_size(int i){
     //return the block size you'd like to use 
     /*add your code here */
-    int res[]={100,126,128,150,200,63}
+    int res[]={100,126,128,150,200,63};
     return res[i];
   
 }
